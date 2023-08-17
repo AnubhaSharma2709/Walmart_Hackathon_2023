6d54e8e046a709fb84a27f402abfc9bd78a0d52e
Anubha Sharma <99638617+AnubhaSharma2709@users.noreply.github.com>
2023-08-17 16:11:16 +0530

    Update walmarthackathon2023.py

--- walmarthackathon2023.py
+++ walmarthackathon2023.py
@@ -9,7 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 import pickle
 from datetime import datetime, timedelta
 from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import train_test_split
 
 # Front Page
 st.title("Walmart Hackathon")
