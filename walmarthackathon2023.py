5b1c7dbd2ad72fda6f5f037f6ecc15199631f903
Anubha Sharma <99638617+AnubhaSharma2709@users.noreply.github.com>
2023-08-17 16:18:15 +0530

    Update walmarthackathon2023.py

--- walmarthackathon2023.py
+++ walmarthackathon2023.py
@@ -295,7 +295,7 @@ elif section == "Warehouse Optimization":
         df.drop(columns=['Timestamp'], inplace=True)
         X = df[['Temperature', 'Pressure', 'Vibration', 'Oil_Level', 'Voltage', 'Current', 'Load', 'Speed']]
         y = df['Maintenance_Required']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=10)
         model = RandomForestClassifier()
         model.fit(X_train, y_train)
         X_test = X_test.sample(n=10, random_state=10)  # Select a subset for testing
